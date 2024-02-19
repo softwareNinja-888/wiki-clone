c32f6aace64b69ed0ee5b44384bd459c12c6ddc6
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-02-19 18:27:22 +0200

    Add entry pages and add links

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
 
 
 def index(request):
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def generate(request,title):
+
+    titleU = title.upper()
+    titleC = title.capitalize()
+    contentU = util.get_entry(titleU)
+    contentC = util.get_entry(titleC)
+
+    if titleU == "CSS" or titleU == "HTML":
+        return render(request,f"encyclopedia/{titleU}.html",{
+            "content": contentU
+        })
+    else:
+        if contentC == None:
+            return render(request, f"encyclopedia/error.html")
+        return render(request, f"encyclopedia/{titleU}.html",{
+            "content": contentC
+        })
+        
+
+
+
+
+ 
